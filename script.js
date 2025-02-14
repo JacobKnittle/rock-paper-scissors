cf652847ff70b94dba29589c4d74d3e8f4579ef8
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-14 13:56:37 -0800

    add getting computer and human choice along with start calculating round functionality

--- script.js
+++ script.js
@@ -1,25 +1,71 @@
 // What inputs will your program have? Will the user enter data or will you get input from somewhere else?
 
-    // user enters a string of rock paper or scissors
+// user enters a string of rock paper or scissors
 
 // What’s the desired output?
 
-    // the winner will be determined and outputted in the console after 5 rounds of rock paper scissors
+// the winner will be determined and outputted in the console after 5 rounds of rock paper scissors
 
 // Given your inputs, what are the steps necessary to return the desired output?
 
 // getComputerChoice will randomly return either “rock”, “paper” or “scissors”
-    
-    // use math.random to get a random number between 0 - 1 divided into three parts
+function getComputerChoice() {
+  // use math.random to get a random number between 0 - 1 divided into three parts
+  let randomNumber = Math.random();
 
-// getHumanChoice will prompt the use for a choice and return that choice
+  // depending on the number a certain choice will be selected and returned
+  if (randomNumber <= 0.33) {
+    return "rock";
+  } else if (randomNumber <= 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+
+// getHumanChoice will prompt the use for a choice and return that choice cap insensitive
+function getHumanChoice() {
+  return prompt("Rock Paper or Scissors? ").toLowerCase();
+}
+
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
 
 // create a humanScore and a computerScore in the global scope set to 0
+let humanScore = 0;
+let computerScore = 0;
 
 // create a function playRound with two parameters humanChoice and computerChoice that plays one round
+function playRound(humanChoice, computerChoice) {
+  // console.log the winner of the match based on having conditionals for all outcomes while also incrementing the human or computer score
 
-    // make the humanChoice parameter case-insensitive
+  // human chooses rock outcomes
+  if (humanChoice === "rock" && computerChoice === "paper") {
+    console.log("Computer wins!");
+    computerScore++;
+  } else if (humanChoice === "rock" && computerChoice === "rock") {
+    console.log("Tie!");
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("Human wins!");
+    humanScore++;
+  }
 
-    // console.log the winner of the match based on having conditionals for all outcomes while also incrementing the human or computer score
+  // human chooses paper outcomes
+  else if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("Human Wins!");
+    humanScore++;
+  } else if (humanChoice === "paper" && computerChoice === "paper") {
+    console.log("Tie!");
+  } else if (humanChoice === "paper" && computerChoice === "scissors") {
+    console.log("Computer Wins!");
+    computerScore++;
+  }
+  console.log(humanScore, computerScore);
+}
+
+playRound(humanChoice, computerChoice);
 
 // create a function playGame that calls playRound 5 times and move the score variables within the playGame function
