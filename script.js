f4a4c8dceb336134a53f92c6e917875d999f76ae
Jacob Knittle <jacobdknittle@gmail.com>
2025-02-14 12:56:25 -0800

    add pseudocode for the rock paper scissors game

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+// What inputs will your program have? Will the user enter data or will you get input from somewhere else?
+
+    // user enters a string of rock paper or scissors
+
+// What’s the desired output?
+
+    // the winner will be determined and outputted in the console after 5 rounds of rock paper scissors
+
+// Given your inputs, what are the steps necessary to return the desired output?
+
+// getComputerChoice will randomly return either “rock”, “paper” or “scissors”
+    
+    // use math.random to get a random number between 0 - 1 divided into three parts
+
+// getHumanChoice will prompt the use for a choice and return that choice
+
+// create a humanScore and a computerScore in the global scope set to 0
+
+// create a function playRound with two parameters humanChoice and computerChoice that plays one round
+
+    // make the humanChoice parameter case-insensitive
+
+    // console.log the winner of the match based on having conditionals for all outcomes while also incrementing the human or computer score
+
+// create a function playGame that calls playRound 5 times and move the score variables within the playGame function
