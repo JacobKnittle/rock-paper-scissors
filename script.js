638e484e23579b65ce11b0da89a4eb4d46e2352f
JacobKnittle <jacobdknittle@gmail.com>
2026-08-27 15:30:34 -0700

    Add pseudocode for application

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log('hello world')
+// goal is to play rock paper scissors against computer
+// there will be a function that randomly returns the choices
+
+// create a function named getComputerChoice gets a random choice from computer
+    // generate a random number either 1, 2, or 3
+    // write a conditional for each choice based on a number
+    // console.log the return value to test to see if it works properly
+    // return the string for the choice if it matches
+
+// write a function based on getting the humans choice
+    // prompt the user for a choice of the three (assume valid choice)
+    // console.log to test for valid choice is being  
+    // return the choice as a string
+
+// create a variable humanScore for keeping track of their score starting at 0
+
+// create a variable for computerScore for keep track of their score starting at 0
+
+// game will be played one round at a time
+// create function playRound that takes the human and computers choices as arguments
+    // make humanChoice case insensitive
+    // play a single round
+    // increment winners score
+    // console.log the winner of the round
+
+// write function playGame that calls playRound 5 times
+// move the playRound function within the playGame
+// move the score variables inside playGame
+// make a loop that runs 5 times on playGame
